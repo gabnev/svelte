7800a003b73e08e071169771e5e63115a99882da
Gabriel Neves <gabriel.bneves@gmail.com>
2020-11-09 09:22:33 +0000

    svelte

--- budget-calculator/src/App.svelte
+++ budget-calculator/src/App.svelte
@@ -1,7 +1,28 @@
 <script>
-	import Navbar from './Navbar.svelte'
-	import Title from './Title.svelte'
+	// bringing setContext from svelte
+	import {setContext} from 'svelte'
+
+	// Components
+	import Navbar from './Navbar.svelte';
+	import ExpensesList from './ExpensesList.svelte';
+	
+	// Data
+	import expensesData from './expenses';
+
+	// Variables & functions
+	let expenses = [...expensesData];
+
+	function removeExpense(id) {
+		expenses = expenses.filter(expense => expense.id != id)
+		console.log(expenses)
+	}
+
+	// Creating the setContext content
+	setContext('remove', removeExpense)
 </script>
 
 <Navbar />
-<Title />
+
+<main class="content">
+	<ExpensesList {expenses} />
+</main>
---
+++ budget-calculator/src/ExpensesList.svelte
@@ -0,0 +1,23 @@
+<script>
+  import SectionTitle from "./Title.svelte";
+  import Expense from "./Expense.svelte";
+  export let expenses = [];
+  // export let removeExpense;
+</script>
+
+<style>
+  h2 {
+    text-transform: capitalize;
+  }
+</style>
+
+<section>
+  <SectionTitle title="Expense list" />
+  <ul>
+    {#each expenses as expense}
+      <Expense {...expense} />
+    {:else}
+      <h2>no expenses right now</h2>
+    {/each}
+  </ul>
+</section>
